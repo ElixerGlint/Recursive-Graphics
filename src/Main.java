35bad4b6d321427f9afc2504b64d4c6af379dcf1
Elixer <everett.rumery@gmail.com>
2025-04-28 13:53:30 -0400

    asgasgasg

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,14 @@ public class Main {
 		window.add(gamepanel);
 		window.setVisible(true);
 
+		JFrame settings = new JFrame();
+		settings.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		settings.setResizable(false);
+		settings.setTitle("settings");
+		settings.setSize(1000, 800);
+		Settings settingspanel = new Settings();
+		settings.add(settingspanel);
+		settings.setVisible(true);
 
 		
 		window.repaint();
---
+++ src/Settings.java
@@ -0,0 +1,25 @@
+﻿import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import javax.imageio.ImageIO; //using collections
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Settings extends JPanel {
+
+
+    
+
+
+    public void paintComponent(Graphics g) { //paints everything
+		super.paintComponent(g);
+    }
+
+
+
+    
+
+
+}
