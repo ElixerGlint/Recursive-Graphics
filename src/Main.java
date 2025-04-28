8c886b1a409228f16f9bb97442cb864d76b6f059
Elixer <everett.rumery@gmail.com>
2025-04-28 13:34:11 -0400

    sagasg

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import javax.swing.JFrame;
+
+public class Main {
+	public static void main(String[] args) throws InterruptedException {
+		JFrame window = new JFrame();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setTitle("Recursion");
+		window.setSize(1000, 800);
+		Screen gamepanel = new Screen();
+		window.add(gamepanel);
+		window.setVisible(true);
+
+
+		while(true) {
+			window.repaint();
+			// Thread.sleep(4);
+		}
+	}
+}
---
+++ src/Screen.java
@@ -0,0 +1,26 @@
+﻿import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import javax.imageio.ImageIO; //using collections
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Screen extends JPanel {
+
+
+
+
+
+
+
+
+
+    public void paintComponent(Graphics g) { //paints everything
+		super.paintComponent(g);
+        g.fillRect(100, 100, 200, 200);
+    }
+
+
+}
